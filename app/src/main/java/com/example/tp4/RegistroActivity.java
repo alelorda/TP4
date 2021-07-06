9a9fed7587cd71a420fcbbf549c9aeedbc8890e5
alelo <alexis.lorda@gmail.com>
2021-07-06 19:16:43 -0300

    OK...

--- app/src/main/java/com/example/tp4/RegistroActivity.java
+++ app/src/main/java/com/example/tp4/RegistroActivity.java
@@ -11,20 +11,21 @@ import android.widget.EditText;
 
 public class RegistroActivity extends AppCompatActivity {
 
-    private EditText editTextCorreo,editTextPasswordRegistro,editTextNombreCompleto,editTextIngresarUsuario, editTextRepetirPasswordRegistro;
+    private EditText editTextCorreo,editTextPasswordRegistro,editTextNombreCompleto,editTextIngresarUsuario, editTextRepetirPasswordRegistro,editTextemail;
     private Button btnRegistrar2, btnIngresar2;
     SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        editTextCorreo = findViewById(R.id.editTextIngresarUsuario);
+        editTextCorreo = findViewById(R.id.editTextemail);
         editTextPasswordRegistro = findViewById(R.id.editTextRepetirPasswordRegistro);
-        editTextNombreCompleto = findViewById(R.id.editTextIngresarUsuario);
-        editTextIngresarUsuario = findViewById(R.id.editTextIngresarUsuario);
+        editTextNombreCompleto = findViewById(R.id.editTextemail);
+        editTextIngresarUsuario = findViewById(R.id.editTextemail);
         btnRegistrar2 = findViewById(R.id.btnRegistrar2);
         btnIngresar2 = findViewById(R.id.btnRegistrar2);
-        editTextRepetirPasswordRegistro = findViewById(R.id.editTextIngresarUsuario);
+        editTextRepetirPasswordRegistro = findViewById(R.id.editTextemail);
+        editTextemail = findViewById(R.id.editTextemail);
         pref = getSharedPreferences("his_pref", Context.MODE_PRIVATE);
 
         btnRegistrar2.setOnClickListener(new View.OnClickListener() {
@@ -34,12 +35,14 @@ public class RegistroActivity extends AppCompatActivity {
 
                 String correo = editTextCorreo.getText().toString();
                 String pass = editTextPasswordRegistro.getText().toString();
+                String email = editTextemail.getText().toString();
                 String nombre = editTextNombreCompleto.getText().toString();
                 String usuario = editTextIngresarUsuario.getText().toString();
                 String reppass = editTextRepetirPasswordRegistro.getText().toString();
 
                 editor.putString("correo",correo);
                 editor.putString("pass",pass);
+                editor.putString("email",email);
                 editor.putString("nombre",nombre);
                 editor.putString("usuario",usuario);
                 editor.putString("reppass",reppass);
